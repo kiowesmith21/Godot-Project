d0dadc460fe8952726482c4d561a7fa4fce14b55
Kiowe Smith <kiowesmith@gmail.com>
2021-07-18 20:48:07 -0400

    HeartUI

--- Stats.gd
+++ Stats.gd
@@ -1,13 +1,19 @@
 extends Node
 
-export(int) var max_health = 4
-onready var health = max_health
+export(int) var max_health = 4 setget set_max_health
+var health = max_health setget set_health
 
 export(int) var curr_wpn = 1
 onready var wpn_choice = curr_wpn
 
 signal no_health
 signal health_changed(value)
+signal max_health_changed(value)
+
+func set_max_health(value):
+	max_health = value
+	self.health = min(health, max_health)
+	emit_signal("max_health_changed", max_health)
 
 func set_health(value):
 	health = value
@@ -20,4 +26,7 @@ signal wpn_change(choice)
 func set_weapon(choice):
 	wpn_choice = choice
 	emit_signal("wpn_change", wpn_choice)
+	
+func _ready():
+	self.health = max_health
 
