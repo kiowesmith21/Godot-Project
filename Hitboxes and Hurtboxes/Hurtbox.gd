b2c55a37dad65846c56850e0a9f2042a7b0ce040
Kiowe Smith <kiowesmith@gmail.com>
2021-07-19 20:47:16 -0400

    fix attack collision and add git ignore

--- Hitboxes and Hurtboxes/Hurtbox.gd
+++ Hitboxes and Hurtboxes/Hurtbox.gd
@@ -1,2 +1,28 @@
 extends Area2D
 
+var invincible = false setget set_invincible
+
+onready var timer = $Timer
+
+signal invincibility_started
+signal invincibility_ended
+
+func set_invincible(value):
+	invincible = value 
+	if invincible == true:
+		emit_signal("invincibility_started")
+	else:
+		emit_signal("invincibility_ended")
+
+func start_invincibility(duration):
+	self.invincible = true
+	timer.start(duration)
+
+func _on_Timer_timeout():
+	self.invincible = false
+
+func _on_Hurtbox_invincibility_started():
+	set_deferred("monitorable", false)
+
+func _on_Hurtbox_invincibility_ended():
+	monitorable = true
