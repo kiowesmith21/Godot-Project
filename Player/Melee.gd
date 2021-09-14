ff063a11b2ebd678ec7e95e72a6d46f7066c78ec
Kiowe Smith <kiowesmith@gmail.com>
2021-09-14 15:16:33 -0400

    fixed melee and arrow

--- Player/Melee.gd
+++ Player/Melee.gd
@@ -1,20 +1,21 @@
 extends Area2D
 
-var speed = 160
+var speed = 300
 var direction : Vector2
 var atk_dmg
 var timer = Timer.new()
+var Player
 
 var knockback_vector = Vector2.ZERO
 
+func _ready():
+	$Sprite.rotation += Player.position.angle_to_point(get_local_mouse_position())
 
 func _process(delta):
 	#gets the position of the mouse for the fireball so the fireball follows the mouse
 	direction = (get_global_mouse_position() - position).normalized()
 	position = position + speed * delta * direction
 	knockback_vector = direction
-	
-
 	
 func Timer():
 	yield(get_tree().create_timer(0.3), "timeout")
