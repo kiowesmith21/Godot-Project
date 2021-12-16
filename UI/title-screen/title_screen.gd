4e54652e687a68762eb3c80b0dabb7c4a1467da5
Kiowe Smith <kiowesmith@gmail.com>
2021-12-16 17:56:26 -0500

    added sound

--- UI/title-screen/title_screen.gd
+++ UI/title-screen/title_screen.gd
@@ -6,9 +6,11 @@ var scene_path_to_load
 func _ready():
 	for button in $Menu/CenterRow/Buttons.get_children():
 		button.connect("pressed", self, "on_button_pressed", [button.scene_to_load])
+		$TitleMusic.play()
 		
 func on_button_pressed(scene_to_load):
 	scene_path_to_load = scene_to_load
+	$TitleMusic.stop()
 	$FadeIn.show()
 	$FadeIn.fade_in()
 
