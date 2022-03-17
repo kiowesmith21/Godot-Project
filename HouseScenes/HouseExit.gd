70444e80bcc162697c3d34c3a2290c0bdcd5a4df
Solomon Lucas <soluke22@gmail.com>
2022-03-17 14:12:36 -0400

    removed house exits

--- HouseScenes/HouseExit.gd
+++ HouseScenes/HouseExit.gd
@@ -4,7 +4,7 @@ extends Area2D
 onready var scene = get_node("res://World.tscn")
 export (Vector2) var move_to_position
 
-func _on_HouseExit1_body_entered(body):
+func _on_HouseExit_body_entered(body):
 	if body.name == "Player":
 		print("area entered")
 		get_tree().change_scene_to(scene)
--- World.gd
+++ World.gd
@@ -10,6 +10,9 @@ extends Node2D
 func _ready():
 	if PlayerStats.player_position:
 		get_node("Player").global_position = PlayerStats.player_position
+		
+func _process(delta):
+	print("player is in scene")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
