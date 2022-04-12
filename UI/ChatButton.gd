40e954817b366e6a78fe389a95d03d3beb31c9f5
DPrice29 <dnprice@aggies.ncat.edu>
2022-04-12 02:21:23 -0400

    Adding intro text scene and updating Future Merlin dialogue

---
+++ UI/ChatButton.gd
@@ -0,0 +1,7 @@
+extends Button
+
+#export(String) var scene_to_load
+
+func _pressed():
+	get_tree().change_scene("res://World.tscn")
+
--- UI/intro_screen.gd
+++ UI/intro_screen.gd
@@ -1,16 +1,21 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var scene_path_to_load
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for button in $Panel/Button.get_children():
+		button.connect("pressed", self, "on_button_pressed", [button.scene_to_load])
+		$TitleMusic.play()
+		
+func on_button_pressed(scene_to_load):
+	scene_path_to_load = scene_to_load
+	$TitleMusic.stop()
+	$FadeIn.show()
+	$FadeIn.fade_in()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_FadeIn_fade_finished():
+	get_tree().change_scene(scene_path_to_load)
+
+
