8bd816732154844c77ed1bd4adb7c8ba9fabc475
Solomon Lucas <soluke22@gmail.com>
2022-04-12 14:19:17 -0400

    fixed the pause menu

--- UI/Pause Menu/PauseMenu.gd
+++ UI/Pause Menu/PauseMenu.gd
@@ -1,29 +1,26 @@
-extends Popup
-
+extends Control
 
 onready var player = get_node("/root/World/Player/Player")
 var already_paused
-var selectedMenu
-
 
 func _ready():
-	pass
+	hide()
 
 func _input(event):
 	if Input.is_action_just_pressed("menu"):
 		if already_paused:
 			get_tree().paused = false
-			already_paused = false
+			already_paused = get_tree().paused
 			hide()
 		else:
 			# Pause game
-			already_paused = get_tree().paused
 			get_tree().paused = true
+			already_paused = get_tree().paused
 			# Show popup
-			popup()
+			show()
 
 func _on_Resume_pressed():
-	if not already_paused:
+	if already_paused:
 		get_tree().paused = false
 		player.set_process_input(true)
 		hide()
