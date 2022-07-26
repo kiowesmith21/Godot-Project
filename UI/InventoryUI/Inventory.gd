ac8a0d5adc3b4dc6d91707204da5a1b3cf4a56b6
soluke22 <soluke22@gmail.com>
2022-07-26 17:18:20 -0500

    trying to get items to work

--- UI/InventoryUI/Inventory.gd
+++ UI/InventoryUI/Inventory.gd
@@ -4,6 +4,14 @@ extends Control
 var already_pressed
 var free_item = 0
 
+var drag_data = null
+
+signal items_changed(indexes)
+
+export(Array, Resource) var items = [
+	null, null, null, null, null, null, null, null, null,
+]
+
 func _ready():
 	hide()
 
@@ -22,17 +30,47 @@ func _input(_event):
 			# Show popup
 			show()
 
-
 func _on_Button_pressed():
 	hide()
 	already_pressed = false
 	
+func set_item(item_index, item):
+	var previousItem = items[item_index]
+	items[item_index] = item
+	emit_signal("items_changed", [item_index])
+	return previousItem
+
+func swap_items(item_index, target_item_index):
+	var targetItem = items[target_item_index]
+	var item = items[item_index]
+	items[target_item_index] = item
+	items[item_index] = targetItem
+	emit_signal("items_changed", [item_index, target_item_index])
+
+func remove_item(item_index):
+	var previousItem = items[item_index]
+	items[item_index] = null
+	emit_signal("items_changed", [item_index])
+	return previousItem
+
+func make_items_unique():
+	var unique_items = []
+	for item in items:
+		if item is Item:
+			unique_items.append(item.duplicate())
+		else:
+			unique_items.append(null)
+	items = unique_items
 
 func _on_adding_item(item):
 	#set the inventory texture as the pic of the item
-	get_child(0).get_child(0).get_child(0).get_child(1).get_child(free_item).get_child(0).set_texture(item.get_child(0).get_texture())
+	#get_child(0).get_child(0).get_child(0).get_child(1).get_child(free_item).get_child(0).set_texture(item.get_child(0).get_texture())
 	#set the name of the inventory label the same as the item
 	#get_child(0).get_child(0).get_child(0).get_child(1).get_child(free_item).get_child(1)
 	#future stuff to do -> draggable items to move between the inventory and hot bar
-	free_item += 1
+	if(free_item < 9):
+		set_item(free_item,item)
+		free_item += 1
+	else:
+		print("Your inventory is full.")
 
--- UI/InventoryUI/Item.gd
+++ UI/InventoryUI/Item.gd
@@ -5,3 +5,5 @@ class_name Item
 export (String) var name = ""
 export (Texture) var texture
 
+export (String) var Type = ""
+
