9068c0fa9e8bc5e99c2bc4a0dfd106f9857d70f4
DPrice29 <dnprice@aggies.ncat.edu>
2022-08-08 18:57:51 -0400

    Adding dialogue to FMJs

---
+++ Npcs/FutureMerlin/FutureMerlin2.gd
@@ -0,0 +1,58 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var EventTrigger = get_node("EventTrigger")
+onready var FMJSprite = get_node("AnimatedSprite")
+onready var FMJParticles = get_node("CPUParticles2D")
+
+var dialogue_state = 0
+var dialogueUI
+var player
+
+signal FMJAppears
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	dialogueUI = get_tree().root.get_node("/root/World/GameUI/DialogueUI2")
+	player = get_tree().root.get_node("/root/World/Player/Player")
+
+
+func _on_Area2D_area_entered(area):
+	
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		FMJSprite.visible = true
+		FMJParticles.visible = true
+		emit_signal("FMJAppears")
+	pass # Replace with function body.
+
+func talk(choice = ""):
+	#setting the npc's state and name to the dialogueUI
+	dialogueUI.npc = self
+	dialogueUI.npcname = "Future Merlin"
+
+	match dialogue_state:
+		0:
+			dialogue_state = 1
+			dialogueUI.dialogue = "Remember that elder who showed you the way? Yeah, don’t trust that guy. He’s the reason we’re in this whole mess! "
+			dialogueUI.choices = "[Q]  Consider him dead!"
+			dialogueUI.open()
+			print("Next option ", dialogue_state)
+		1:
+			dialogue_state = 2
+			dialogueUI.dialogue = "Calm down sport. For now just do exactly what he tells you."
+			dialogueUI.choices = "[Q] Understood"
+			dialogueUI.open()
+			print("Next option ", dialogue_state)
+		2:
+			dialogueUI.close()
+			dialogue_state = 0
+
+
+
+
