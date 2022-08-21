2361c85a068b2d68fe1f168065f2a33ffb928ca6
Solomon Lucas <soluke22@gmail.com>
2022-08-21 18:41:14 -0500

    fixed item staying in inventory bug

--- UI/InventoryUI/InventorySlot.gd
+++ UI/InventoryUI/InventorySlot.gd
@@ -7,6 +7,8 @@ onready var itemTexture = $ItemTexture
 func display_item(item):
 	if item is Item:
 		itemTexture.texture = item.texture
+	else:
+		itemTexture.texture = $Background
 
 func get_drag_data(_position):
 	var item_index = get_index()
